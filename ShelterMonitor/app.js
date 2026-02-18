a0d300b016627e5f8ec4c65dd97fd0a611d94bb5
Ranshye <sayberdragon@gmail.com>
2026-02-18 06:31:07 +0200

    Added backend connection with DB

--- ShelterMonitor/app.js
+++ ShelterMonitor/app.js
@@ -1,6 +1,25 @@
 import express from 'express';
+import mysql from 'mysql2';
+
+
 const app = express();
-const port = process.env.NODE_DOCKER_PORT || 8080;
+const port = process.env.NODE_DOCKER_PORT || 6969;
+
+let connect = mysql.createConnection({
+    host: process.env.DB_HOST || "mysql",
+    user: process.env.DB_USER || "root",
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT || 3306
+});
+
+connect.connect(function (err) {
+    if (err) {
+        console.error('Error connecting to MySQL: ' + err.stack);
+        return;
+    }
+    console.log("connected");
+});
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
@@ -9,3 +28,4 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
+
